1a117bdce8787faa7c179ffaaa71421e14142510
Mark Kozhydlo <99179725+kozhydlo@users.noreply.github.com>
2023-12-19 20:57:23 +0200

    Delete validations directory

--- validations/auth.js
+++
@@ -1,8 +0,0 @@
-import { body } from "express-validator";
-
-export const registerValidation = [
-  body("email", "Неправильний формат пошти").isEmail(),
-  body("password", "Пароль має бати мінімум 5 символів!").isLength({ min: 5 }),
-  body("fullname", "Вкажіть Ім'я").isLength({ min: 3 }),
-  body("avatarUrl", "Неправильне посилання на аватарку").optional().isURL(),
-];
